5781ba57ca97d854e177ef6bf35788e697c9b192
MartaG77 <129276573+MartaG77@users.noreply.github.com>
2023-09-06 21:33:05 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,11 @@
 const categoryList = document.querySelectorAll("#categories li.item");
 const categoryAmount = categoryList.length;
-console.log(`Number of categories: ${categoryAmount}`);
+console.log(`Number of categories:`, categoryAmount);
 categoryList.forEach(category => {
   const categoryTitle = category.querySelector("h2");
   const categoryName = categoryTitle.textContent;
   const listElement = category.querySelectorAll("ul li");
   const elementAmount = listElement.length;
-  console.log(`Category: ${categoryName}`);
-  console.log(`Elements: ${elementAmount}`);
+  console.log(`Category:`, categoryName);
+  console.log(`Elements:`, elementAmount);
 });
